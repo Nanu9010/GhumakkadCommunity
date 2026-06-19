f448bdcb90b320e949e1a6c5a752697bdae70c56
Kartikraj Nalage <kartikrajnalage9010@gmail.com>
2026-06-19 17:47:52 +0530

    Fix Vercel: expose application variable, remove serverless-wsgi

--- api/index.py
+++ api/index.py
@@ -2,7 +2,6 @@ import os
 import sys
 import shutil
 
-# Ensure we're in the right directory and can import Django
 HERE = os.path.dirname(os.path.abspath(__file__))
 DJANGO_ROOT = os.path.join(HERE, 'skytravel')
 
@@ -12,7 +11,6 @@ os.chdir(DJANGO_ROOT)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skytravel.settings')
 os.environ.setdefault('VERCEL', '1')
 
-# Copy DB to /tmp on cold start
 if os.environ.get('VERCEL') == '1':
     src_db = os.path.join(DJANGO_ROOT, 'db.sqlite3')
     dst_db = '/tmp/db.sqlite3'
@@ -20,10 +18,5 @@ if os.environ.get('VERCEL') == '1':
         shutil.copy2(src_db, dst_db)
 
 from django.core.wsgi import get_wsgi_application
-from serverless_wsgi import handle_request
 
-django_app = get_wsgi_application()
-
-
-def handler(request, response):
-    return handle_request(django_app, request, response)
+application = get_wsgi_application()
